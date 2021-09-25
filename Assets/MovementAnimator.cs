8c0d184a155faa275816bca34e2b85bc98042f5d
marcuscastelo <marcuscastelo@usp.br>
2021-09-25 18:47:29 -0300

    Collision tags + sprite flip

--- Assets/MovementAnimator.cs
+++ Assets/MovementAnimator.cs
@@ -6,9 +6,15 @@ public class MovementAnimator : MonoBehaviour
 {
     public Movement movement;
     public Animator animator;
+    public Transform transformToFlip;
 
     private void FixedUpdate() {
         animator.SetFloat("speedX", movement.Velocity.x);
         animator.SetFloat("speedY", movement.Velocity.y);
+        if (movement.Velocity.x < 0) {
+            transformToFlip.localScale = new Vector3(-1, 1, 1);
+        } else if (movement.Velocity.x > 0) {
+            transformToFlip.localScale = new Vector3(1, 1, 1);
+        }
     }
 }
