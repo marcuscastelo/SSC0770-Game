d54c3ea226b3f8e55b5fd32c70f7a4eb26940be4
marcuscastelo <marcuscastelo@usp.br>
2021-09-27 09:54:35 -0300

    Moving files + Adding clock

---
+++ Assets/Clock.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    // public bool paused = false;
+    // public float time = 0;
+    // public float timeScale = 1;
+    public bool late = false; // TODO: remove this and determine if late in the update loop
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("late", late);
+    }
+}
