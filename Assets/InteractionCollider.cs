f95e29c20e6d006c12f3c1d6ca557ce62518f6df
marcuscastelo <marcuscastelo@usp.br>
2021-11-02 20:15:20 -0300

    Fixing tags/layers and friction

--- Assets/InteractionCollider.cs
+++ Assets/InteractionCollider.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class InteractionCollider : MonoBehaviour
 {
+    GameObject lastCollidedObject;
     //TODO: update InteractionController selectedObject to be the object that is being interacted with
 
     //OnTriggerEnter is called when the Collider other enters the trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        //If it is a selectable object (have a Interact tag), set it as the selected object
+    }
+
 }
