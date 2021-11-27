59470ce6e13e5eddf0ef8e5da53bbd2d22b49313
marcuscastelo <marcuscastelo@usp.br>
2021-11-27 20:37:47 -0300

    Next level confirmation dialog

--- Assets/_Project/Prefabs/InteractableObject/SendNextLevelInteractionResponse.cs
+++ Assets/_Project/Prefabs/InteractableObject/SendNextLevelInteractionResponse.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Assertions;
 
 using Hypnos.Core;
 using Zenject;
@@ -6,6 +7,7 @@ using Zenject;
 public class SendNextLevelInteractionResponse : MonoBehaviour, IInteractionResponse
 {
     [SerializeField] private int nextLevel;
+    [SerializeField] private DialogInfo confirmationDialogInfo;
 
     private LevelSwitcher _levelSwitcher;
 
@@ -15,9 +17,38 @@ public class SendNextLevelInteractionResponse : MonoBehaviour, IInteractionRespo
         _levelSwitcher = levelSwitcher;
     }
 
+    public void OnValidate()
+    {
+        Assert.IsTrue(nextLevel > 0);
+        if (confirmationDialogInfo != null)
+            Assert.AreEqual(confirmationDialogInfo.buttons, DialogButtonCombination.YesNo, "Only Yes/No buttons are supported for this interaction response");
+    }
+
+    void Awake()
+    {
+        if (confirmationDialogInfo == null) {
+            confirmationDialogInfo = ScriptableObject.CreateInstance<DialogInfo>();
+            confirmationDialogInfo.title = "Go to next level?";
+            confirmationDialogInfo.content = "Are you sure you want to go to the next level?";
+            confirmationDialogInfo.buttons = DialogButtonCombination.YesNo;
+        }
+    }
+
     public void OnInteracted(Interaction interaction)
     {
-        _levelSwitcher.SwitchToLevel(nextLevel);
-        interaction.EndInteraction(true);
+        Dialog dialog = new Dialog(confirmationDialogInfo, (pressedButton) =>
+        {
+            if (pressedButton == DialogButton.Yes)
+            {
+                _levelSwitcher.SwitchToLevel(nextLevel);
+                interaction.EndInteraction(true);
+            }
+            else
+            {
+                interaction.EndInteraction(false);
+            }
+        });
+
+        DialogSystem.ShowDialog(dialog);
     }
 }
